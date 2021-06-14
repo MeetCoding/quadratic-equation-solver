6204dd87d99929063632ac2d46cbe4204a2dcd0d
MeetCoding <meet.prakash.shah@gmail.com>
2021-06-14 10:11:53 +0530

    Added Error on non-perfect quadratic eq.

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ document.getElementById('solve').addEventListener('click',()=> {
     let xVal1 = document.getElementById('xVal1');
     let xVal2 = document.getElementById('xVal2');
 
-    if(terms.includes(NaN)) {
-        document.getElementById('error').style.visibility = 'visible';
-        xVal1.style.visibility = 'hidden';
-        xVal2.style.visibility = 'hidden';
-    } else {
+    if(terms.includes(NaN)) throwError
+    else {
         document.getElementById('error').style.visibility = 'hidden';
         let ans = solve(terms);
+        if(ans==undefined) throwError();
         xVal1.style.visibility = 'visible';
         xVal2.style.visibility = 'visible';
         xVal1.innerHTML = `First Root = ${ans[0]}`;
         xVal2.innerHTML = `Second Root = ${ans[1]}`;
     }
 })
+
+function throwError() {
+    document.getElementById('error').style.visibility = 'visible';
+    xVal1.style.visibility = 'hidden';
+    xVal2.style.visibility = 'hidden';
+}
